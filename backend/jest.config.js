670f4e97f80147a330cbfd1372138b6303140428
musa <ST10293362@vcconnect.edu.za>
2026-02-21 19:55:33 +0200

    builds

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -2,5 +2,6 @@ module.exports = {
   testEnvironment: 'node',
   coverageDirectory: 'coverage',
   collectCoverageFrom: ['src/**/*.js', '!src/server.js'],
+  coverageReporters: ['lcov', 'text', 'text-summary'],
   testMatch: ['**/tests/**/*.test.js', '**/*.test.js']
 };
