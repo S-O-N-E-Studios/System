73e1654b9fe59d0412d6752327b9f587da13b67c
musa <ST10293362@vcconnect.edu.za>
2026-03-07 20:33:43 +0200

    ui v3.0.6

--- frontend/src/test-setup.ts
+++ frontend/src/test-setup.ts
@@ -7,5 +7,40 @@ class ResizeObserver {
   disconnect() {}
 }
 
-Object.defineProperty(globalThis, 'ResizeObserver', { value: ResizeObserver, writable: true });
+Object.defineProperty(globalThis, 'ResizeObserver', {
+  value: ResizeObserver,
+  writable: true,
+});
+
+// Polyfill canvas getContext for libraries like lottie-web that expect a 2D context.
+// JSDOM doesn't implement HTMLCanvasElement.prototype.getContext, so we provide a minimal stub.
+const canvasContextStub: Partial<CanvasRenderingContext2D> = {
+  fillStyle: null,
+  fillRect: () => {},
+  clearRect: () => {},
+  getImageData: () => ({ data: [] } as ImageData),
+  putImageData: () => {},
+  createLinearGradient: () => ({
+    addColorStop: () => {},
+  }) as unknown as CanvasGradient,
+  drawImage: () => {},
+  save: () => {},
+  restore: () => {},
+  beginPath: () => {},
+  moveTo: () => {},
+  lineTo: () => {},
+  closePath: () => {},
+  stroke: () => {},
+  translate: () => {},
+  scale: () => {},
+  rotate: () => {},
+  arc: () => {},
+  fill: () => {},
+  measureText: () => ({ width: 0 } as TextMetrics),
+};
+
+Object.defineProperty(HTMLCanvasElement.prototype, 'getContext', {
+  value: () => canvasContextStub,
+  writable: true,
+});
 
